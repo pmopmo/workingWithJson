3fcc5707327f4a10ebea37dc48fe9ecaac27ec26
petmo996 <peter.moller@gmail.com>
2021-03-22 20:18:24 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,61 @@
+// Working with json
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// This 'type' was created by using http://json2struct.mervine.net
+	type Forecast struct {
+		ApprovedTime string `json:"approvedTime"`
+		Geometry     struct {
+			Coordinates [][]float64 `json:"coordinates"`
+			Type        string      `json:"type"`
+		} `json:"geometry"`
+		ReferenceTime string `json:"referenceTime"`
+		TimeSeries    []struct {
+			Parameters []struct {
+				Level     int64     `json:"level"`
+				LevelType string    `json:"levelType"`
+				Name      string    `json:"name"`
+				Unit      string    `json:"unit"`
+				Values    []float64 `json:"values"`
+			} `json:"parameters"`
+			ValidTime string `json:"validTime"`
+		} `json:"timeSeries"`
+	}
+
+	urlJson := "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json"
+
+	resp, err := http.Get(urlJson)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(bytes))
+	myJsonString := string(bytes)
+
+	var forecast Forecast
+	json.Unmarshal(
+		[]byte(myJsonString),
+		&forecast,
+	)
+
+	fmt.Printf("ApprovedTime: %s, lat: %f, c-type %s",
+		forecast.ApprovedTime,
+		forecast.Geometry.Coordinates[0][0],
+		forecast.Geometry.Type)
+
+
+}
